1064abed8a33c268fb8cf8d9547f8c2a2d163427
Moritz Friedrich <moritz@matchory.com>
2023-03-06 08:47:32 +0100

    Simplified health check

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
 
 set -e
-
-role=${CONTAINER_ROLE:-web}
-
-if [ "$role" = "web" ]; then
-    exec curl -f http://localhost:2114/health?plugin=http || exit 1
-elif [ "$role" = "worker" ]; then
-    # TODO: Does a worker health check make sense?
-    exit 0
-else
-    exit 0
-fi
+exec curl -f http://localhost:2114/health?plugin=http || exit 1
